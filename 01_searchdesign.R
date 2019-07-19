f3234aa00ece365dda8ca364a39d4d07486359cb
Nicholas Moran <nicholaspatrickmoran@gmail.com>
2019-07-19 13:41:11 +0200

    final searches and processing

--- 01_searchdesign.R
+++ 01_searchdesign.R
@@ -1,38 +1,14 @@
 ##########_____Mutualism/Antagonism Synthesis Review_____#########
 
 
-library(devtools)
-install_github("mjwestgate/revtools")
-install.packages("revtools")
-install.packages("dplyr")
+#library(devtools)
+#install_github("mjwestgate/revtools")
+#install.packages("revtools")
+#library(xtable)
+#library(slam)
 library(dplyr)
 library(operators)
 library(revtools)
-
-install.packages("xtable")
-library(xtable)
-install.packages("slam")
-library(slam)
-install.packages("revtools")
-library(dplyr)
-install.packages("revtools")
-library(dplyr)
-install.packages("revtools")
-library(dplyr)
-install.packages("revtools")
-library(dplyr)
-install.packages("revtools")
-library(dplyr)
-install.packages("revtools")
-library(dplyr)
-install.packages("revtools")
-library(dplyr)
-install.packages("revtools")
-library(dplyr)
-
-
-
-
 
 
 #Databases: Web of Science Core Collection (http://www.webofknowledge.com); Scopus (https://www.scopus.com/)
@@ -62,7 +38,6 @@ Scopus <- read_bibliography("scopus.bib")
 Scopus.df <- as.data.frame(Scopus)
 Scopus.df.reduced <- Scopus.df[,reducing.fields]
 summary(Scopus.df.reduced)
-write.csv(Scopus.df.reduced,"Scopus.df.reduced.csv", row.names=FALSE)
 
 #290 Results
 
@@ -80,82 +55,91 @@ MA.fullrec.duplicates <- find_duplicates(data = MA.fullrec,
 MA.fullrec.deduplicated.auto <- extract_unique_references(MA.fullrec, MA.fullrec.duplicates)
 nrow(MA.fullrec.deduplicated.auto); summary(MA.fullrec.deduplicated.auto)
 
-#gives ... results (number of deduplicates removed)
+#gives 415 results (140 duplicates removed)
 
-#exporting csv for manual deduplication
+#exporting csv of deduplicated database for manual deduplication
 write.csv(MA.fullrec.deduplicated.auto,"MA.fullrec.deduplicated.auto.csv", row.names=FALSE)
 
-#gives ... results (number of additional duplicates removed)
+#exporting csv of pre-auto duplication database to manually check if find_duplicates function is operating correctly
+write.csv(MA.fullrec,"MA.fullrec.csv", row.names=FALSE) # function is operating well, all duplicates checked were real duplicates
+
+#additional duplicates removed from file
+#Competition versus cooperation: success of individuals foraging alone and in groups	Ranta, E. and Rita, H. and Lindstrom, American Naturalist K.10.1086/285528
+#Are there general principles of signal design?	Dawkins, M.S.	Philosophical Transactions - Royal Society of London, B 10.1098/rstb.1993.0065
+#Viral quasispecies profiles as the result of the interplay of competition cooperation	Arbiza, Juan and Mirazo, Santiago and Fort, Hugo	BMC EVOLUTIONARY BIOLOGY 10.1186/1471-2148-10-137
+#On the morphology of Balaenophilus manatorum (Ortz, Lalana, and Torres) (Copepoda: Harpacticoida) from sea turtles of the Mexican pacific with notes on intraspecific variation	Surez-Morales, E. and Lazo-Wasem, E.A.	Comparative Parasitology 10.1654/4351.1
+
+
+#gives 411 results (number of additional duplicates removed)
 
 
 #Production of files for individual screeners:
 #importing full database
-MA.fullrec.final <- read.csv("MA.fullrec.deduplicated.final.csv", strip.white = TRUE)
+MA.fullrec.final <- read.csv("MA.fullrec.deduplicated.manual.csv", strip.white = TRUE)
+nrow(MA.fullrec.final)
 
 #adding an abstract id for each record
-MA.fullrec.final <- transform(MA.fullrec.final, AbstractID=paste("MA",1:265))
+MA.fullrec.final <- transform(MA.fullrec.final, AbstractID=paste("MA",1:411))
 MA.fullrec.final$AbstractID <- gsub(" ", "", MA.fullrec.final$AbstractID) 
 
+#Adding Columns for screeners to complete
 MA.fullrec.final$"screener initials" <- ""
 MA.fullrec.final$"include or exclude (note: exclude only if the abstract is unrelated to our topic, if you are unsure mark as include)" <- ""
 MA.fullrec.final$"ecological scale (e.g. interspecific or intraspecific)" <- ""
-MA.fullrec.final$"topic area (e.g. predator-prey, parasite-host, male-female)" <- ""
+MA.fullrec.final$"topic area (e.g. predator-prey, parasite-host, male-female, theoretical/modelling, review)" <- ""
 
 #for full double screening, the final deduplicated database is randomly split into 5 random sections twice for 10 total screeners
 MA.fullrec.screendatA <- MA.fullrec.final 
 MA.fullrec.screendatB <- MA.fullrec.final
 
-#to split  MA.fullrec.screendatA into 5 equal sections of 53 (UPDATE WITH THE FULL LIST)
+#to split MA.fullrec.screendatA into 5 sections (4x 82, 1 x 83) 
 set.seed(253)
-MA.fullrec.screendatA.pt1 <- sample_n(MA.fullrec.screendatA, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatA.pt1 <- sample_n(MA.fullrec.screendatA, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatA <- subset(MA.fullrec.screendatA, AbstractID %!in% MA.fullrec.screendatA.pt1$AbstractID)
 nrow(MA.fullrec.screendatA)
 
-MA.fullrec.screendatA.pt2 <- sample_n(MA.fullrec.screendatA, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatA.pt2 <- sample_n(MA.fullrec.screendatA, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatA <- subset(MA.fullrec.screendatA, AbstractID %!in% MA.fullrec.screendatA.pt2$AbstractID)
 nrow(MA.fullrec.screendatA)
 
-MA.fullrec.screendatA.pt3 <- sample_n(MA.fullrec.screendatA, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatA.pt3 <- sample_n(MA.fullrec.screendatA, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatA <- subset(MA.fullrec.screendatA, AbstractID %!in% MA.fullrec.screendatA.pt3$AbstractID)
 nrow(MA.fullrec.screendatA)
 
-MA.fullrec.screendatA.pt4 <- sample_n(MA.fullrec.screendatA, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatA.pt4 <- sample_n(MA.fullrec.screendatA, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatA <- subset(MA.fullrec.screendatA, AbstractID %!in% MA.fullrec.screendatA.pt4$AbstractID)
 nrow(MA.fullrec.screendatA)
 
-MA.fullrec.screendatA.pt5 <- sample_n(MA.fullrec.screendatA, 53, replace = FALSE, weight = NULL)
-MA.fullrec.screendatA <- subset(MA.fullrec.screendatA, AbstractID %!in% MA.fullrec.screendatA.pt5$AbstractID)
-nrow(MA.fullrec.screendatA)
+MA.fullrec.screendatA.pt5 <- MA.fullrec.screendatA
 
-#to split  MA.fullrec.screendatB into 5 equal sections of 53 (UPDATE WITH THE FULL LIST)
+#to split MA.fullrec.screendatB into 5 sections (4x 82, 1 x 83) 
 set.seed(23)
-MA.fullrec.screendatB.pt1 <- sample_n(MA.fullrec.screendatB, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatB.pt1 <- sample_n(MA.fullrec.screendatB, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatB <- subset(MA.fullrec.screendatB, AbstractID %!in% MA.fullrec.screendatB.pt1$AbstractID)
 nrow(MA.fullrec.screendatB)
 
-MA.fullrec.screendatB.pt2 <- sample_n(MA.fullrec.screendatB, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatB.pt2 <- sample_n(MA.fullrec.screendatB, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatB <- subset(MA.fullrec.screendatB, AbstractID %!in% MA.fullrec.screendatB.pt2$AbstractID)
 nrow(MA.fullrec.screendatB)
 
-MA.fullrec.screendatB.pt3 <- sample_n(MA.fullrec.screendatB, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatB.pt3 <- sample_n(MA.fullrec.screendatB, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatB <- subset(MA.fullrec.screendatB, AbstractID %!in% MA.fullrec.screendatB.pt3$AbstractID)
 nrow(MA.fullrec.screendatB)
 
-MA.fullrec.screendatB.pt4 <- sample_n(MA.fullrec.screendatB, 53, replace = FALSE, weight = NULL)
+MA.fullrec.screendatB.pt4 <- sample_n(MA.fullrec.screendatB, 82, replace = FALSE, weight = NULL)
 MA.fullrec.screendatB <- subset(MA.fullrec.screendatB, AbstractID %!in% MA.fullrec.screendatB.pt4$AbstractID)
 nrow(MA.fullrec.screendatB)
 
-MA.fullrec.screendatB.pt5 <- sample_n(MA.fullrec.screendatB, 53, replace = FALSE, weight = NULL)
-MA.fullrec.screendatB <- subset(MA.fullrec.screendatB, AbstractID %!in% MA.fullrec.screendatB.pt5$AbstractID)
-nrow(MA.fullrec.screendatB)
+MA.fullrec.screendatB.pt5 <- MA.fullrec.screendatB
 
-write.csv(MA.fullrec.screendatA.pt1, "MA.fullrec.screendatA.pt1.csv")
-write.csv(MA.fullrec.screendatA.pt2, "MA.fullrec.screendatA.pt2.csv")
-write.csv(MA.fullrec.screendatA.pt3, "MA.fullrec.screendatA.pt3.csv")
-write.csv(MA.fullrec.screendatA.pt4, "MA.fullrec.screendatA.pt4.csv")
-write.csv(MA.fullrec.screendatA.pt5, "MA.fullrec.screendatA.pt5.csv")
-write.csv(MA.fullrec.screendatB.pt1, "MA.fullrec.screendatB.pt1.csv")
-write.csv(MA.fullrec.screendatB.pt2, "MA.fullrec.screendatB.pt2.csv")
-write.csv(MA.fullrec.screendatB.pt3, "MA.fullrec.screendatB.pt3.csv")
-write.csv(MA.fullrec.screendatB.pt4, "MA.fullrec.screendatB.pt4.csv")
-write.csv(MA.fullrec.screendatB.pt5, "MA.fullrec.screendatB.pt5.csv")
+write.csv(MA.fullrec.screendatA.pt1, "./MA_screening_files/MA.fullrec.screendatA.pt1.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatA.pt2, "./MA_screening_files/MA.fullrec.screendatA.pt2.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatA.pt3, "./MA_screening_files/MA.fullrec.screendatA.pt3.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatA.pt4, "./MA_screening_files/MA.fullrec.screendatA.pt4.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatA.pt5, "./MA_screening_files/MA.fullrec.screendatA.pt5.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatB.pt1, "./MA_screening_files/MA.fullrec.screendatB.pt1.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatB.pt2, "./MA_screening_files/MA.fullrec.screendatB.pt2.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatB.pt3, "./MA_screening_files/MA.fullrec.screendatB.pt3.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatB.pt4, "./MA_screening_files/MA.fullrec.screendatB.pt4.csv", row.names=FALSE)
+write.csv(MA.fullrec.screendatB.pt5, "./MA_screening_files/MA.fullrec.screendatB.pt5.csv", row.names=FALSE)
+
---
+++ 02_abstractscreening.R
@@ -0,0 +1,7 @@
+##########_____Mutualism/Antagonism Synthesis Review_____#########
+
+
+#Screeners:
+#Nicholas Moran (NM)
+#Uli Ernst (UE)
+#Jamie Winternitz (JW)
