3a10dcd1e044b41e183f4f65cc7d271370eb24b5
Nicholas Moran <nicholaspatrickmoran@gmail.com>
2019-09-23 08:27:40 +1000

    screening progress

--- 03_abstractscreeningreturns.R
+++ 03_abstractscreeningreturns.R
@@ -9,34 +9,37 @@ library(operators)
 #KJM
 MA.fullrec.screendatA.pt1.done <- read.csv("./MA_screening_returns/MA.fullrec.screendatA.pt1.done.csv", strip.white = TRUE)
 summary(MA.fullrec.screendatA.pt1.done)
-KJM.included<-subset(MA.fullrec.screendatA.pt1.done, decision == "include")
-nrow(KJM.included) #31/82 included, 37.80488% inclusion rate
-summary(KJM.included)
+nrow(subset(MA.fullrec.screendatA.pt1.done, decision == "include")) #31/82 included, 37.80488% inclusion rate
+summary(subset(MA.fullrec.screendatA.pt1.done, decision == "include"))
 
 #UE
 
 
 #JW
-
+MA.fullrec.screendatA.pt3.done <- read.csv("./MA_screening_returns/MA.fullrec.screendatA.pt3.done.csv", strip.white = TRUE)
+summary(MA.fullrec.screendatA.pt3.done)
+nrow(subset(MA.fullrec.screendatA.pt3.done, decision == "include")) #63/82 included, 76.82927% inclusion rate
+summary(subset(MA.fullrec.screendatA.pt3.done, decision == "include"))
 
 #CM
 MA.fullrec.screendatA.pt4.done <- read.csv("./MA_screening_returns/MA.fullrec.screendatA.pt4.done.csv", sep = ";", strip.white = TRUE)
 summary(MA.fullrec.screendatA.pt4.done)
-CM.included<-subset(MA.fullrec.screendatA.pt4.done, decision == "include")
-nrow(CM.included) #39/82 included, 47.56098% inclusion rate
-summary(CM.included)
+nrow(subset(MA.fullrec.screendatA.pt4.done, decision == "include")) #39/82 included, 47.56098% inclusion rate
+summary(subset(MA.fullrec.screendatA.pt4.done, decision == "include"))
 
 #CF
 
 
 #PT
-
+MA.fullrec.screendatB.pt1.done <- read.csv("./MA_screening_returns/MA.fullrec.screendatB.pt1.done.csv", strip.white = TRUE)
+summary(MA.fullrec.screendatB.pt1.done)
+nrow(subset(MA.fullrec.screendatB.pt1.done, decision == "In")) #74/137 studies included, 54.0146% inclusion rate
+summary(subset(MA.fullrec.screendatB.pt1.done, decision == "In"))
 
 #MW
 MA.fullrec.screendatB.pt2.done <- read.csv("./MA_screening_returns/MA.fullrec.screendatB.pt2.done.csv", strip.white = TRUE)
 summary(MA.fullrec.screendatB.pt2.done)
-MW.included<-subset(MA.fullrec.screendatB.pt2.done, decision == "include")
-nrow(MW.included) #42/137 studies included, 30.65693% inclusion rate
-summary(MW.included)
+nrow(subset(MA.fullrec.screendatB.pt2.done, decision == "include")) #42/137 studies included, 30.65693% inclusion rate
+summary(subset(MA.fullrec.screendatB.pt2.done, decision == "include"))
 
 #NM
